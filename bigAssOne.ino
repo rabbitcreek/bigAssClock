b2d2afbf12982be38b0ed31de24e1f0ba41bc613
rabbitcreek <rabbitcreek@gmail.com>
2025-05-20 19:57:19 -0800

    Add files via upload

--- bigAssOne.ino
+++ bigAssOne.ino
@@ -3,11 +3,13 @@
 
 
 
-//#include <Servo.h>
+#include "FastLED.h"
 #include <ESP32Servo.h>
 Servo myservo;
 #include <Wire.h> 
-
+#define NUM_LEDS 35
+CRGB leds[NUM_LEDS];
+#define PIN 15 
 #include <SPI.h> // Required for RTClib to compile properly
 #include <RTClib.h> // From https://github.com/millerlp/RTClib
 // Real Time Clock setup
@@ -43,11 +45,11 @@ DateTime now; // define variable to hold date and time
 bool hiLow;
 DateTime intoTime;
 void servoMove(){
-   myservo.attach(D10,600,2400);//important if you want your servo to do a maximum 180 degrees--most have their querks
+   //important if you want your servo to do a maximum 180 degrees--most have their querks
   delay(200);
   myservo.writeMicroseconds(servoWrite);
-  delay(8000);
-  myservo.detach();
+  delay(1000);
+  //myservo.detach();
   
 }
 
@@ -61,27 +63,41 @@ void armMove(DateTime now, DateTime futureHigh, DateTime futureLow,int dS){
   if(hiLow)nextTime = futureHigh;
   else nextTime = futureLow;
   hourFuture = nextTime.hour();
-  hourFuture = hourFuture+dS;
+  hourFuture = hourFuture+1;
   if(nextTime.hour()>11)hourFuture=hourFuture-12;
   if(hiLow==0)hourFuture=hourFuture+6;//this uses only one side to pointer to set high and low tides
   if(hourFuture>11)hourFuture=hourFuture-12;
+  for(hourFuture = 0; hourFuture < 13; hourFuture++){
+  
   servoWrite=(hourFuture*10)+((nextTime.minute()*10)/60);
   Serial.print(servoWrite);
-  Serial.print("servoWrite");
-  servoWrite=map(servoWrite,0,120,1810,940);//each servo is different--these are the micorseconds limits for mine
-  servoWrite=constrain(servoWrite,700,2000);
- servoMove();
+  Serial.print("servoWriteOne");
+  
+  servoWrite=map(servoWrite,0,120,1175,1395);//each servo is different--these are the micorseconds limits for mine
+  servoWrite=constrain(servoWrite,1175,1395);
+  Serial.print("servoWriteTwo:");
+  Serial.print(servoWrite);
+  myservo.writeMicroseconds(servoWrite);
+  delay(1000);
+  //servoMove();
+  }
 }
 void setup(){
   Wire.begin();  
   RTC.begin();
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(1000);
  //RTC.adjust(DateTime(F(__DATE__), F(__TIME__))); 
   //RTC.adjust(DateTime(2019,10,1,7,30,0)); 
   // Start up the oled display
-   
+   ESP32PWM::allocateTimer(0);
+  myservo.setPeriodHertz(50);
+   FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+   myservo.attach(13,600,2400);
 }
 void loop(){
+  delay(8000);
+Serial.print("im here");
   dS = 0;
   now = RTC.now();
   if((now.month()>3&&now.month()<11)||(now.month()==3&&now.day()>11)||(now.month()==11&&now.day()<6))dS=1;
@@ -143,7 +159,7 @@ void loop(){
   Serial.print("/");
   Serial.print(intoTime.day(), DEC); 
   Serial.print("  ");
-   Serial.print(intoTime.hour(), DEC); 
+   Serial.print((intoTime.hour() +1), DEC); 
   Serial.print(":");
   if (intoTime.minute() < 10) {
     Serial.print("0");
@@ -164,9 +180,68 @@ void loop(){
   while(intoTime.unixtime() >= now.unixtime() ){
     now = RTC.now();    
     //outPut(now, intoTime, dS);
+    Serial.print("servoWriteThree: ");
+    Serial.println(servoWrite);
     servoMove();
     now = (now.unixtime() - dS * 3600);
     delay(60000);
+    lightTime();
   }
 }
-
+void lightTime(){
+  setAll(0,0,0);
+  setAll(250,214,165);
+  timerLight();
+}
+void setAll(byte red, byte green, byte blue) {
+  for(int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue); 
+  }
+  FastLED.show();
+}
+void timerLight() {
+  /*
+   int dS = 0;
+   now = RTC.now();
+   if((now.month()<3||now.month()>11)||(now.month()==3&&now.day()<11)||(now.month()==11&&now.day()>6))dS=1;
+   now = (now.unixtime() - dS*3600);
+   int hourTime = now.hour();
+   Serial.print("now.hour");
+   Serial.print(hourTime);
+  if(now.hour()>11)hourTime = hourTime - 12;
+   hourTime = (hourTime * 9) +((now.minute() * 9) / 60);
+   hourTime = hourTime + 35;
+   Serial.print("hourTime");
+   Serial.println(hourTime);
+   if (hourTime >= 104) hourTime = hourTime - 104;
+   if (hourTime == 0) hourTime = 1;
+   */
+   now = RTC.now();
+   if((now.month()<3||now.month()>11)||(now.month()==3&&now.day()<11)||(now.month()==11&&now.day()>6))dS=1;
+   now = (now.unixtime() - dS*3600);
+   int hourTime = now.hour();
+   Serial.print("now.hour");
+   Serial.print(hourTime);
+  if(now.hour()>11)hourTime = hourTime - 12;
+   hourTime = (hourTime * 3)+ ((now.minute() * 3) / 60);
+   
+   Serial.print("hourTime");
+   Serial.println(hourTime);
+   
+   leds[hourTime - 1] = CRGB::Red; 
+   leds[hourTime] = CRGB::Red; 
+   leds[hourTime + 1] = CRGB::Red; 
+   FastLED.show();
+}
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H 
+   // FastLED
+   leds[Pixel].r = red;
+   leds[Pixel].g = green;
+   leds[Pixel].b = blue;
+ #endif
+}
